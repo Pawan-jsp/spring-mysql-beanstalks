6a5497ae5fc0c4897df3d7472df737b2250eb0eb
Pawan <101386982+Pawan-JPG@users.noreply.github.com>
2024-02-04 13:10:17 +0530

    first commits

---
+++ src/main/java/com/example/springmysqlbeanstalks/SpringMysqlBeanstalksApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmysqlbeanstalks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlBeanstalksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMysqlBeanstalksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springmysqlbeanstalks/SpringMysqlBeanstalksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmysqlbeanstalks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlBeanstalksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
